23dcefdc67c698538134b1cfaca46cda3a81977e
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-26 20:12:23 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -2,128 +2,167 @@ from userbot.events import register
 from userbot import CMD_HELP, bot
 
 
-@register(outgoing=True, pattern=r"^\.stopvc (?:(now)|(.*) - (.*))")
-async def _(e):
+@register(outgoing=True, pattern=r"^\.joinvc (?:(now)|(.*) - (.*))")
+async def joinvc(msg: Message):
+    """ join voice chat """
+    global CHAT_NAME, CHAT_ID  # pylint: disable=global-statement
+
+    await msg.delete()
+
+    if CHAT_NAME:
+        await reply_text(msg, f"`Already joined in {CHAT_NAME}`")
+        return
+
+    CHAT_ID = msg.chat.id
+    CHAT_NAME = msg.chat.title
     try:
-        await e.client(stopvc(await get_call(e)))
-        await eor(e, "`Voice Chat Stopped...`")
-    except Exception as ex:
-        await eor(e, f"`{str(ex)}`")
+        await call.start(CHAT_ID)
+    except RuntimeError:
+        try:
+            peer = await msg.client.resolve_peer(CHAT_ID)
+            await msg.client.send(
+                functions.phone.CreateGroupCall(
+                    peer=peer, random_id=2
+                )
+            )
+            await asyncio.sleep(3)
+            await call.start(CHAT_ID)
+        except Exception as err:
+            await msg.err(str(err))
+            CHAT_ID, CHAT_NAME = 0, ""
+
+
+@register(outgoing=True, pattern=r"^\.leavevc (?:(now)|(.*) - (.*))")
+@vc_chat
+async def leavevc(msg: Message):
+    """ leave voice chat """
+    global CHAT_NAME, CHAT_ID  # pylint: disable=global-statement
+
+    await msg.delete()
+
+    if CHAT_NAME:
+        await call.stop()
+        await asyncio.sleep(2)
+        CHAT_NAME = ""
+        CHAT_ID = 0
+    else:
+        await reply_text(msg, "`I didn't find any Voice-Chat to leave")
+
+
 
 
 @register(outgoing=True, pattern=r"^\.playvc (?:(now)|(.*) - (.*))")
-async def _(e):
-    zz = await eor(e, "`VC bot started...`")
-    er, out = await bash("npm start")
-    LOGS.warning(er)
-    LOGS.info(out)
-    await zz.edit(f"Failed {er}")
+@vc_chat
+async def play_music(msg: Message):
+    """ play music in voice chat """
 
+    if not CHAT_ID or msg.chat.id != CHAT_ID:
+        return
 
-@register(outgoing=True, pattern=r"^\.vcinvite (?:(now)|(.*) - (.*))")
-async def _(e):
-    ok = await eor(e, "`Inviting Members to Voice Chat...`")
-    users = []
-    z = 0
-    async for x in e.client.iter_participants(e.chat_id):
-        if not x.bot:
-            users.append(x.id)
-    hmm = list(user_list(users, 6))
-    for p in hmm:
-        try:
-            await e.client(invitetovc(call=await get_call(e), users=p))
-            z += 6
-        except BaseException:
-            pass
-    await ok.edit(f"`Invited {z} users`")
-
-
-@register(outgoing=True, pattern=r"^\.startvc (?:(now)|(.*) - (.*))")
-async def _(e):
-    try:
-        await e.client(startvc(e.chat_id))
-        await eor(e, "`Voice Chat Started...`")
-    except Exception as ex:
-        await eor(e, f"`{str(ex)}`")
-
-
-@register(outgoing=True, pattern=r"^\.listvcaccess (?:(now)|(.*) - (.*))")
-async def _(e):
-    xx = await eor(e, "`Getting Voice Chat Bot Users List...`")
-    mm = get_vcsudos()
-    pp = f"**{len(mm)} Voice Chat Bot Approved Users**\n"
-    if len(mm) > 0:
-        for m in mm:
-            try:
-                name = (await e.client.get_entity(int(m))).first_name
-                pp += f"• [{name}](tg://user?id={int(m)})\n"
-            except ValueError:
-                pp += f"• `{int(m)} » No Info`\n"
-    await xx.edit(pp)
-
-
-@register(outgoing=True, pattern=r"^\.rmvcacces (?:(now)|(.*) - (.*))")
-async def _(e):
-    xx = await eor(e, "`Disapproving to access Voice Chat features...`")
-    input = e.pattern_match.group(1)
-    if e.reply_to_msg_id:
-        userid = (await e.get_reply_message()).sender_id
-        name = (await e.client.get_entity(userid)).first_name
-    elif input:
-        try:
-            userid = await get_user_id(input)
-            name = (await e.client.get_entity(userid)).first_name
-        except ValueError as ex:
-            return await eod(xx, f"`{str(ex)}`", time=5)
+    if msg.input_str:
+        if re.match(yt_regex, msg.input_str):
+            QUEUE.append(msg)
+            if PLAYING:
+                text = SCHEDULED.format(
+                    title="Song", link=msg.input_str, position=len(QUEUE))
+                await reply_text(msg, text)
+        else:
+            mesg = await reply_text(msg, f"Searching `{msg.input_str}` on YouTube")
+            title, link = await _get_song(msg.input_str)
+            if link:
+                await mesg.delete()
+                mesg = await reply_text(msg, f"Found [{title}]({link})")
+                QUEUE.append(mesg)
+                if PLAYING:
+                    text = SCHEDULED.format(
+                        title=title, link=mesg.link, position=len(QUEUE))
+                    await reply_text(msg, text)
+            else:
+                await mesg.edit("No results found.")
+    elif msg.reply_to_message and msg.reply_to_message.audio:
+        QUEUE.append(msg)
+        replied = msg.reply_to_message
+        if PLAYING:
+            text = SCHEDULED.format(
+                title=replied.audio.title, link=replied.link, position=len(QUEUE))
+            await reply_text(msg, text)
     else:
-        return await eod(xx, "`Reply to user's msg or add it's id/username...`", time=3)
-    if not is_vcsudo(userid):
-        return await eod(
-            xx,
-            f"[{name}](tg://user?id={userid})` is not approved to use my Voice Chat Bot.`",
-            time=5,
-        )
-    try:
-        del_vcsudo(userid)
-        await eod(
-            xx,
-            f"[{name}](tg://user?id={userid})` is removed from Voice Chat Bot Users.`",
-            time=5,
-        )
-    except Exception as ex:
-        return await eod(xx, f"`{str(ex)}`", time=5)
+        return await reply_text(msg, "Input not found")
+
+    if not PLAYING:
+        await handle_queue()
 
 
-@register(outgoing=True, pattern=r"^\.vcaccess (?:(now)|(.*) - (.*))")
-async def _(e):
+@register(outgoing=True, pattern=r"^\.queue (?:(now)|(.*) - (.*))")
+@vc_chat
+async def view_queue(msg: Message):
+    """ View Queue """
 
+    await msg.delete()
 
-async def _(e):
-    xx = await eor(e, "`Approving to access Voice Chat features...`")
-    input = e.pattern_match.group(1)
-    if e.reply_to_msg_id:
-        userid = (await e.get_reply_message()).sender_id
-        name = (await e.client.get_entity(userid)).first_name
-    elif input:
-        try:
-            userid = await get_user_id(input)
-            name = (await e.client.get_entity(userid)).first_name
-        except ValueError as ex:
-            return await eod(xx, f"`{str(ex)}`", time=5)
+    if not QUEUE:
+        out = "`Queue is empty`"
     else:
-        return await eod(xx, "`Reply to user's msg or add it's id/username...`", time=3)
-    if is_vcsudo(userid):
-        return await eod(
-            xx,
-            f"[{name}](tg://user?id={userid})` is already approved to use my Voice Chat Bot.`",
-            time=5,
-        )
-    try:
-        add_vcsudo(userid)
-        await eod(
-            xx,
-            f"[{name}](tg://user?id={userid})` is added to Voice Chat Bot Users.`",
-            time=5,
-        )
-    except Exception as ex:
-        return await eod(xx, f"`{str(ex)}`", time=5)
+        out = f"**{len(QUEUE)} Songs in Queue:**\n"
+        for i in QUEUE:
+            replied = i.reply_to_message
+            if replied and replied.audio:
+                out += f"\n - [{replied.audio.title}]"
+                out += f"({replied.link})"
+            else:
+                link = i.input_str
+                if "Found" in i.text:
+                    link = i.entities[0].url
+                out += f"\n{link}"
+
+    await reply_text(msg, out)
+
+
+@register(outgoing=True, pattern=r"^\.skipsong (?:(now)|(.*) - (.*))")
+@vc_chat
+async def skip_music(msg: Message):
+    """ skip music in vc """
+
+    await msg.delete()
+
+    await _skip()
+    await reply_text(msg, "`Skipped`")
+
+
+@register(outgoing=True, pattern=r"^\.pause (?:(now)|(.*) - (.*))")
+@vc_chat
+async def pause_music(msg: Message):
+    """ paise music in vc """
+
+    await msg.delete()
+
+    call.pause_playout()
+    await reply_text(msg, "⏸️ **Paused** Music Successfully")
+
+
+
+@register(outgoing=True, pattern=r"^\.resume (?:(now)|(.*) - (.*))")
+@vc_chat
+async def resume_music(msg: Message):
+    """ resume music in vc """
+
+    await msg.delete()
+
+    call.resume_playout()
+    await reply_text(msg, "◀️ **Resumed** Music Successfully")
+
+@register(outgoing=True, pattern=r"^\.stopmusic (?:(now)|(.*) - (.*))")
+@vc_chat
+async def stop_music(msg: Message):
+    """ stop music in vc """
+
+    await msg.delete()
+    await _skip(True)
+
+    await reply_text(msg, "`Stopped Userge-Music.`")
+
+CMD_HELP.update(
+    {
+        "vc": ".vc\
+    \nBroadcast ke Seluruh Grup."
+    })
