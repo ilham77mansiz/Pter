477950617ee5446bc8ed9421af72f128a547face
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-06 08:32:49 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -319,7 +319,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_message(BOTLOG_CHATID, "```🎸PETERCORD-USERBOT🎸\nMENYALA```")
+    await bot.send_message(BOTLOG_CHATID, "```KEONG\nMENYALA```")
     return
 
 with bot:
